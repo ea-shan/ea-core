b3187ec0a8aebaae1ad261478113c06c3f0d11e9
ea-shan <shantanu.goswami@expressanalytics.net>
2025-05-15 19:35:28 +0530

    Latest changes to megamenu

--- plugins/greenshift-smart-code-ai/build/indexgs.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c306546213b6eb828d39');
--- plugins/vertical-sidebar-menu-block/build/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array(), 'version' => '0656384b7f96a688b6ff');
